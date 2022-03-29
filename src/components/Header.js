0064306de8a6b4812edd40aa7fb5f8938257e99d
kaung-myat-khaing <kaung8701@gmail.com>
2022-03-29 20:17:26 +0630

    remove course info

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ export default function Header() {
 				<img src={headerImg} alt="troll face" />
 				<span className="heading-text">Meme Generator</span>
 			</h3>
-			<h3 className="currentCourse d-none d-sm-block">
+			{/* <h3 className="currentCourse d-none d-sm-block">
 				<span className="main-course">react course</span> -{" "}
 				<span className="project-no">project 3</span>
-			</h3>
+			</h3> */}
 		</header>
 	);
 }
